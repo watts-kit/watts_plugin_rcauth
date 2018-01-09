51538eaa0628a48952ad7e45730bb0202846895c
Marcus <marcus@hardt-it.de>
2018-01-09 14:30:30 +0100

    set SSL_METHOD to TLSv1_2_METHOD

--- plugin/capilot/myproxy/client.py
+++ plugin/capilot/myproxy/client.py
@@ -329,7 +329,8 @@ reference only
 
     # Parametise SSL METHOD to allow later update if needed - set to TLSv1 to
     # address POODLE vulnerability
-    SSL_METHOD = SSL.TLSv1_METHOD
+    # Fix marcus uros mischa: FIXME
+    SSL_METHOD = SSL.TLSv1_2_METHOD
 
     MYPROXY_SERVER_ENVVARNAME = 'MYPROXY_SERVER'
     MYPROXY_SERVER_PORT_ENVVARNAME = 'MYPROXY_SERVER_PORT'
